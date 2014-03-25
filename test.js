eb7fed025c5079c3575ea3995b79aac29421cebe
Thomas Jensen <tj@inze.it>
2014-03-25 22:23:17 +0100

    Add test to cover using rename-key in a prototype

--- test.js
+++ test.js
@@ -5,38 +5,54 @@ var renameKey = require('./');
 var test = require('tap').test;
 
 test('rename-key', function( t ) {
-		var o = {
-			'name': 'GitHub',
-			'url': 'https://github.com',
-		};
+	var o = {
+		'name': 'GitHub',
+		'url': 'https://github.com',
+	};
 
-		var r = renameKey(o, 'url', 'web');
+	var r = renameKey(o, 'url', 'web');
 
-		t.deepEqual(r, {
-			'name': 'GitHub',
-			'web': 'https://github.com',
-		});
+	t.deepEqual(r, {
+		'name': 'GitHub',
+		'web': 'https://github.com',
+	});
 
-		t.deepEqual(o, {
-			'name': 'GitHub',
-			'web': 'https://github.com',
-		});
+	t.deepEqual(o, {
+		'name': 'GitHub',
+		'web': 'https://github.com',
+	});
 
-		o.renameKey = renameKey;
+	o.renameKey = renameKey;
 
-		r = o.renameKey('name', 'title');
+	r = o.renameKey('name', 'title');
 
-		t.deepEqual(r, {
-			'title': 'GitHub',
-			'web': 'https://github.com',
-			'renameKey': renameKey,
-		});
+	t.deepEqual(r, {
+		'title': 'GitHub',
+		'web': 'https://github.com',
+		'renameKey': renameKey,
+	});
 
-		t.deepEqual(o, {
-			'title': 'GitHub',
-			'web': 'https://github.com',
-			'renameKey': renameKey,
-		});
+	t.deepEqual(o, {
+		'title': 'GitHub',
+		'web': 'https://github.com',
+		'renameKey': renameKey,
+	});
 
-		t.end();
+	var Base = function(){};
+	Base.prototype.renameKey = renameKey;
+
+	var Person = function( name, surname ){ this.name = name; this.surname = surname; };
+	Person.prototype = Object.create(Base.prototype);
+
+	var person = new Person('Sofie', 'Jensen');
+	person
+		.renameKey('name', 'firstName')
+		.renameKey('surname', 'lastName');
+
+	t.deepEqual(person, {
+		'firstName': 'Sofie',
+		'lastName': 'Jensen',
+	});
+
+	t.end();
 });
